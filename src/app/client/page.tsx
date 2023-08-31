e1dc9ef59a9cf3eda12cdb25ef86d8f62a2dafb9
Nico Albanese <gcalbanese96@gmail.com>
2023-08-31 15:26:36 +0100

    added to client too

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 
 export default function Client() {
   const { data } = trpc.users.getUsers.useQuery();
+  const { data: cars } = trpc.cars.getCars.useQuery();
   return (
     <main>
       <Link href="/">Server</Link>
       <h1>Hello</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(cars, null, 2)}</pre>
     </main>
   );
 }
